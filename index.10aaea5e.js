ce54e6e5a2af18fd72d5a6f23c88f0ebe1ecaa30
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-01-17 21:41:11 +0000

    Deploying to gh-pages from @ SpaceVasyl/goit-js-hw-10@26cc4ec4acc4d6af71c4d5532ab696ea44f5ac1a 🚀

--- index.10aaea5e.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#search-box");document.querySelector(".country-info");e.addEventListener("input",(function(e){fetch("https://restcountries.com/v2/all?fields=name,capital,currencies").then((e=>{console.log(e)}))}));
-//# sourceMappingURL=index.10aaea5e.js.map
--- index.92980843.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("#search-box");document.querySelector(".country-info");e.addEventListener("input",(function(e){fetch("https://restcountries.com/v2/all?fields=name,capital,currencies").then((function(e){console.log(e)}))}))}();
-//# sourceMappingURL=index.92980843.js.map
