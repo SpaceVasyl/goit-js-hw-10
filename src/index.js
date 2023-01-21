a5f0a4799455f907f7909a51a326f47c8b0824e7
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-01-21 19:03:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ const DEBOUNCE_DELAY = 300;
 const textInput = document.querySelector('#search-box');
 const output = document.querySelector(".country-info");
 
-textInput.addEventListener("input", printOutput);
-function printOutput(event) {
-    fetch('https://restcountries.com/v3.1/all').then(data=>{
-   console.log(data); 
-})
-}
-
+// textInput.addEventListener("input", printOutput);
+// function printOutput(event) {
+   
+// }
+fetch('https://restcountries.com/v3.1/name/{name}').then(response=>{
+   console.log(response.json()); 
+}) 
 
 
 // output.textContent = event.currentTarget.value;
