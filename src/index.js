002bbb5add8bd8e5576e1b1de53ddeb8e0b1e399
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-01-22 13:14:39 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 import './css/styles.css';
 const DEBOUNCE_DELAY = 300;
 const textInput = document.querySelector('#search-box');
-// const output = document.querySelector(".country-info");
+const output = document.querySelector(".country-info");
 
-textInput.addEventListener("input", printOutput);
-function printOutput(event) {
+textInput.addEventListener("input", fetchCountries);
+function fetchCountries(event) {
     // console.log(event.target.value);
     fetch(`https://restcountries.com/v2/name/${event.target.value}?fields=name,capital,population,flags,languages`)
     .then((response) => {
-        console.log(response.json());
-      return response.json();
+      return printOutput(response.json());
     })
+    .then((country) => {
+        console.log(country);
+      })
     .catch((error) => {
       console.log(error);
     });
 }
 
+function printOutput (response){
 
-// .then((country) => {
-    //   console.log(country);
-    // })
-// output.textContent = event.currentTarget.value;
+}
+
+
