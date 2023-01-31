36ccccb507acf3282f3fa04f32cb6464692db21d
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-02-01 01:18:56 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,11 +2,12 @@ import {debounce, throttle} from 'debouncing';
 import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 500;
 const textInput = document.querySelector('#search-box');
+const output = document.querySelector(".country-list");
 
 export function fetchCountries(event) {
     fetch(`https://restcountries.com/v2/name/${event.target.value}?fields=name,capital,population,flags,languages`)
     .then((res) => res.json())
-    .then((data) => data.length > 10 ? tooMuchCountries(data) : data.length > 1 ? listOFCountries(data): alert(data))
+    .then((data) => data.length > 10 ? tooMuchCountries(data) : data.length > 1 ? listOFCountries(data): displayCountrie(data))
 }
 textInput.addEventListener("input", debounce(fetchCountries, DEBOUNCE_DELAY));
 
@@ -15,7 +16,12 @@ function tooMuchCountries(data){
   };
 
 function listOFCountries(data){
-    console.log(data);
+    const nameCountry = null;
+    for(let i = 0; i<data.length; i++){
+        console.log(data[i].name);
+    }
   }
 
-
+function displayCountrie(data) {
+    console.log(data);
+}
