c718704f4064fdba4da2626a754d9109a9fabdfe
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-01-31 23:19:00 +0200

    b

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,16 +1,21 @@
 import {debounce, throttle} from 'debouncing';
-const DEBOUNCE_DELAY = 300;
+import Notiflix from 'notiflix';
+const DEBOUNCE_DELAY = 500;
 const textInput = document.querySelector('#search-box');
 
 export function fetchCountries(event) {
     fetch(`https://restcountries.com/v2/name/${event.target.value}?fields=name,capital,population,flags,languages`)
     .then((res) => res.json())
-    .then((data) => tooMuchCountries(data))
+    .then((data) => data.length > 10 ? tooMuchCountries(data) : data.length > 1 ? listOFCountries(data): alert(data))
 }
 textInput.addEventListener("input", debounce(fetchCountries, DEBOUNCE_DELAY));
 
 function tooMuchCountries(data){
-    if (data.length > 10) {
-      alert("Too many matches found. Please enter a more specific name.")
-    }
+    Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
   };
+
+function listOFCountries(data){
+    console.log(data);
+  }
+
+
