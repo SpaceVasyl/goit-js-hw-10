2a9fc0c8b953f5c0bd10eecdde66be1b5e0fd2b5
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-02-03 02:23:13 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -13,18 +13,14 @@ export function fetchCountries(event) {
 textInput.addEventListener("input", debounce(fetchCountries, DEBOUNCE_DELAY));
 
   function tooMuchCountries(data){
-  output.remove();
-  countryInfo.remove();
   Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+  return
   };
 
   function listOFCountries(data){
-  countryInfo.remove();
-  output.remove();
-  const nameCountry = null;
-  
+  countryInfo.innerHTML = "";
+  output.innerHTML = "";
   for(let i = 0; i<data.length; i++){
-  console.log(data[i]);
   const lastItem = document.createElement("li");
   lastItem.style.listStyle = "none";
   lastItem.style.display = "flex";
@@ -33,15 +29,17 @@ textInput.addEventListener("input", debounce(fetchCountries, DEBOUNCE_DELAY));
   lastItem.innerHTML = `<img src="${data[i].flags.svg}" height='18'style="margin: 1px 0px;padding-top:6px;"><p style="margin: 0px; padding-bottom:1px;">${data[i].name}</p>`;
   output.append(lastItem);
   }
+  return
   }
 
 function displayCountry({name,capital,population,flags,languages}) {
-  console.log({name,capital,population,flags,languages});
-  output.remove();
-  countryInfo.remove();
-  countryInfo.innerHTML = `<h2 style="font-size:40px"><img src="${flags.svg}" height='18'style="margin-bottom: 5px;margin-right: 5px;">${name}</h2>
+  countryInfo.innerHTML = "";
+  output.innerHTML = "";
+  
+ return countryInfo.innerHTML = `<h2 style="font-size:40px"><img src="${flags.svg}" height='18'style="margin-bottom: 5px;margin-right: 5px;">${name}</h2>
   <ul style="list-style:none ;">
   <li>Capital:${capital}</li>
   <li>Population:${population}</li>
   <li>Languages:${[languages.name]}</li>
-  </ul>`}
+  </ul>`;
+}
