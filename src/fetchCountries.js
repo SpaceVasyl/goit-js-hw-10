438f5bce5edb06f51a5c262739e50ec3cac535ff
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-02-06 03:45:48 +0200

    asd

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,4 @@
-import {debounce, throttle} from 'debouncing';
-import Notiflix from 'notiflix';
-const DEBOUNCE_DELAY = 500;
 const ENDPOINT = "https://restcountries.com/v2";
-const textInput = document.querySelector('#search-box');
 const output = document.querySelector(".country-list");
 const countryInfo = document.querySelector(".country-info");
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import './css/styles.css';
 import FETCH from './fetchCountries.js';
-// import Notiflix from 'notiflix';
+import {debounce, throttle} from 'debouncing';
+import Notiflix from 'notiflix';
+const DEBOUNCE_DELAY = 500;
+const form = document.querySelector('#search-box');
+const output = document.querySelector(".country-list");
+const countryInfo = document.querySelector(".country-info");
 
-FETCH.fetchCountries("ukraine").then(console.log);
+form.addEventListener("input", debounce(searchCountries, DEBOUNCE_DELAY));
+function searchCountries(e) {
+    e.preventDefault();
+    const inputCountry = e.target.value.trim();
+    if(!inputCountry){
+        return
+    }
+    FETCH.fetchCountries(inputCountry).then((c)=>{if (c.length === 0) throw new Error("No data"); createMarkup(c);
+});
+}
+
+function createMarkup(c){
+{}
+    console.log();
+output.innerHTML = `
+<li>
+<p>${(as)}</p>
+</li>
+`}
