930b65a064cf5deaf9c64935d82523b7ff1eff29
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-02-06 14:07:53 +0200

    aaaaa

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,12 @@
+import Notiflix from "notiflix";
 const ENDPOINT = "https://restcountries.com/v2";
 const output = document.querySelector(".country-list");
 const countryInfo = document.querySelector(".country-info");
 
-function fetchCountries(query) {
-   return fetch(`${ENDPOINT}/name/${query}`)
-  .then((res)=> res.json());
+ export function fetchCountries(query) {
+  return fetch(`${ENDPOINT}/name/${query}`)
+  .then((res)=> res.json())
+  .catch((error)=> Notiflix.Notify.error("Oops, there is no country with that name"));
 }
 
-export default {fetchCountries};
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './css/styles.css';
-import FETCH from './fetchCountries.js';
+import {fetchCountries} from './fetchCountries.js';
 import {debounce, throttle} from 'debouncing';
 import Notiflix from 'notiflix';
-const DEBOUNCE_DELAY = 500;
+const DEBOUNCE_DELAY = 300;
 const form = document.querySelector('#search-box');
 const output = document.querySelector(".country-list");
 const countryInfo = document.querySelector(".country-info");
@@ -10,19 +10,41 @@ const countryInfo = document.querySelector(".country-info");
 form.addEventListener("input", debounce(searchCountries, DEBOUNCE_DELAY));
 function searchCountries(e) {
     e.preventDefault();
-    const inputCountry = e.target.value.trim();
+    const inputCountry = e.target.value;
     if(!inputCountry){
-        return
+        return;
     }
-    FETCH.fetchCountries(inputCountry).then((c)=>{if (c.length === 0) throw new Error("No data"); createMarkup(c);
-});
+    fetchCountries(inputCountry).then((c)=>createMarkup(c))
 }
 
 function createMarkup(c){
-{}
-    console.log();
-output.innerHTML = `
+if (c.length >10) {
+    Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+}
+else if (c.length >1 && c.length<10){
+output.innerHTML = c.map((a)=>{
+const  {name, capital, population, flag, languages} = a
+return `
 <li>
-<p>${(as)}</p>
+<img src="${flag}" width=100 height=70></img>
+<p>${name}</p>
 </li>
-`}
+`
+})
+}
+else if (c.length === 1){
+output.innerHTML = c.map((a)=>{
+const  {name, capital, population, flag, languages} = a
+return `
+
+<img src="${flag}" width=100 height=70></img>
+<h2>${name}</h2>
+<p>${capital}</p>
+`
+})
+}
+else if (c){alert("Oops, there is no country with that name")}
+}
+
+
+
